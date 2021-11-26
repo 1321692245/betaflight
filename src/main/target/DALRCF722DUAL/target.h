935ff38d229dcaaae66e54b47f58eff25cb72282
1321692245 <83385130+1321692245@users.noreply.github.com>
2021-11-26 15:32:55 +0800

    Update target.h

--- src/main/target/DALRCF722DUAL/target.h
+++ src/main/target/DALRCF722DUAL/target.h
@@ -20,14 +20,14 @@
 
 #pragma once
 
-#define TARGET_BOARD_IDENTIFIER "DLF7"
-#define USBD_PRODUCT_STRING  "DALRCF722DUAL"
+define TARGET_BOARD_IDENTIFIER "FCF7"
+define USBD_PRODUCT_STRING  "DALRCF722DUAL"
 
 #define ENABLE_DSHOT_DMAR       DSHOT_DMAR_ON
-#define LED0_PIN                PC14
+define LED0_PIN                PA1
 
 #define USE_BEEPER
-#define BEEPER_PIN              PC13
+define BEEPER_PIN              PC4
 #define BEEPER_INVERTED
 
 #define USE_EXTI
@@ -44,11 +44,11 @@
 #define ENSURE_MPU_DATA_READY_IS_LOW
 
 #define USE_GYRO
-#define USE_GYRO_SPI_MPU6000
+define USE_GYRO_SPI_MPU6000
 #define USE_GYRO_SPI_MPU6500
 
 #define USE_ACC
-#define USE_ACC_SPI_MPU6000
+define USE_ACC_SPI_MPU6000
 #define USE_ACC_SPI_MPU6500
 
 #define GYRO_1_ALIGN                CW180_DEG
@@ -58,7 +58,7 @@
 #define GYRO_CONFIG_USE_GYRO_DEFAULT GYRO_CONFIG_USE_GYRO_1 
 
 #define USE_BARO
-#define USE_BARO_MS5611
+define USE_BARO_MS5611
 
 #define USE_MAG
 #define USE_MAG_HMC5883
@@ -71,40 +71,43 @@
 #define USE_UART4
 #define USE_UART5
 
-#define UART1_TX_PIN            PB6
-#define UART1_RX_PIN            PB7
+define UART1_TX_PIN            PA9
+define UART1_RX_PIN            PA10
 
-#define UART2_TX_PIN            PA2
-#define UART2_RX_PIN            PA3
+define UART2_TX_PIN            PA2
+define UART2_RX_PIN            PA3
 
-#define UART3_TX_PIN            PC10
-#define UART3_RX_PIN            PC11
+define UART3_TX_PIN            PB10
+define UART3_RX_PIN            PB11
 
 #define UART4_TX_PIN            PA0
 #define UART4_RX_PIN            PA1
 
 #define UART5_TX_PIN            PC12
 #define UART5_RX_PIN            PD2
+
+define UART6_TX_PIN            PC6
+define UART6_RX_PIN            PC7
 #define SERIAL_PORT_COUNT       6 
 
 #define USE_I2C
 #define USE_I2C_DEVICE_1
 #define I2C_DEVICE              I2CDEV_1
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+define I2C1_SCL                PB6
+define I2C1_SDA                PB7
 
 #define USE_SPI
 
 #define USE_SPI_DEVICE_1 // 2xGYRO/ACC
-#define SPI1_SCK_PIN            PA5
-#define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PA7
+define SPI1_SCK_PIN            PA5
+define SPI1_MISO_PIN           PA6
+define SPI1_MOSI_PIN           PA7
 
 #define USE_SPI_DEVICE_2 // MAX7456
 #define SPI2_NSS_PIN            PB12
-#define SPI2_SCK_PIN            PB13
-#define SPI2_MISO_PIN           PB14
-#define SPI2_MOSI_PIN           PB15
+define SPI2_SCK_PIN            PB13
+define SPI2_MISO_PIN           PB14
+define SPI2_MOSI_PIN           PB15
 
 #define USE_MAX7456
 #define MAX7456_SPI_INSTANCE    SPI2
